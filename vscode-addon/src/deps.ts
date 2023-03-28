b997ec1c72f803bd505fcaa04d39cd7923ff3c3d
Jens Reimann <jreimann@redhat.com>
2023-03-28 14:21:51 +0200

    refactor: handle multiple roots

--- vscode-addon/src/deps.ts
+++ vscode-addon/src/deps.ts
@@ -6,14 +6,14 @@ import {
 } from "./data";
 import { MarkdownString, ThemeIcon } from "vscode";
 
-export class EnforcerDependenciesProvider implements vscode.TreeDataProvider<DependencyNode> {
-    private dependencies: Array<Dependency>;
+export class EnforcerDependenciesProvider implements vscode.TreeDataProvider<TreeNode> {
+    private dependencies: Map<string, Array<Dependency>>;
 
-    private _onDidChangeTreeData: vscode.EventEmitter<DependencyNode | undefined | null | void> = new vscode.EventEmitter<DependencyNode | undefined | null | void>();
-    readonly onDidChangeTreeData: vscode.Event<DependencyNode | undefined | null | void> = this._onDidChangeTreeData.event;
+    private _onDidChangeTreeData: vscode.EventEmitter<TreeNode | undefined | null | void> = new vscode.EventEmitter<TreeNode | undefined | null | void>();
+    readonly onDidChangeTreeData: vscode.Event<TreeNode | undefined | null | void> = this._onDidChangeTreeData.event;
 
     constructor() {
-        this.dependencies = [];
+        this.dependencies = new Map();
     }
 
     refresh(): void {
@@ -21,34 +21,80 @@ export class EnforcerDependenciesProvider implements vscode.TreeDataProvider<Dep
     }
 
     update(update: UpdatedDependencies): void {
-        // FIXME: handle root information
-        this.dependencies = update.dependencies;
-        this.dependencies.sort((a, b) => {
+        update.dependencies.sort((a, b) => {
             return a.purl.localeCompare(b.purl);
-        })
+        });
+        this.dependencies.set(update.root.toString(), update.dependencies);
         this.refresh();
     }
 
-    getChildren(element?: DependencyNode): DependencyNode[] {
+    getChildren(element?: TreeNode): TreeNode[] {
         console.debug("Update:", this.dependencies);
 
+        // eslint-disable-next-line no-constant-condition
+        if (this.dependencies.size < 1 || true) {
+            return this.getChildrenSingle(element);
+        } else {
+            return this.getChildrenMulti(element);
+        }
+    }
+
+    getTreeItem(element: TreeNode): vscode.TreeItem {
+        return element;
+    }
+
+    getChildrenSingle(element?: TreeNode): TreeNode[] {
         if (!element) {
-            // root
-            return this.dependencies.map((entry) => {
+            // root 
+            const list = this.dependencies.values().next().value;
+            if (list === undefined) {
+                return [];
+            }
+            return [...list].map((entry) => {
                 return new DependencyNode(entry, vscode.TreeItemCollapsibleState.None);
             });
         } else {
-            // child
             return [];
         }
     }
 
-    getTreeItem(element: DependencyNode): vscode.TreeItem {
-        return element;
+    getChildrenMulti(element?: TreeNode): TreeNode[] {
+        if (!element) {
+            // root
+            return [...this.dependencies.keys()].map((key) => {
+                return new ProjectNode(key, vscode.TreeItemCollapsibleState.Expanded);
+            });
+        } else if (element instanceof ProjectNode) {
+            return EnforcerDependenciesProvider.mapDependencies(this.dependencies.get(element.root));
+        } else {
+            return [];
+        }
+    }
+
+    static mapDependencies(dependencies: Array<Dependency>): TreeNode[] {
+        if (!dependencies) {
+            return [];
+        }
+        return dependencies.map((entry) => {
+            return new DependencyNode(entry, vscode.TreeItemCollapsibleState.None);
+        });
     }
 }
 
 const ICON_LIBRARY = new ThemeIcon("library");
+const ICON_PROJECT = new ThemeIcon("project");
+
+type TreeNode = DependencyNode | ProjectNode | undefined;
+
+class ProjectNode extends vscode.TreeItem {
+    constructor(
+        public readonly root: string,
+        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    ) {
+        super(root, collapsibleState);
+        this.iconPath = ICON_PROJECT;
+    }
+}
 
 class DependencyNode extends vscode.TreeItem {
     constructor(
